f27719b8de2c5247e3e68929a68468f26678bfef
Omar-Said <omar.a.said@outlook.com>
2021-12-15 12:59:20 -0500

    converted hardcoded components to dynamic components to display the cards on the web app

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./homepage.styles.sass";
+
+import Directory from "../../components/menu-item/directory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
